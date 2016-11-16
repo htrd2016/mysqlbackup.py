336be7910cef3854b32cb2003bf619b053380bc8
siryang2006 <393121997@qq.com>
2016-11-16 17:41:51 +0800

    Create run-new.sh

---
+++ run-new.sh
@@ -0,0 +1,1 @@
+nohup python /root/zabbix_database_backup/mysqlbackup.py 127.0.0.1 username password 3306 zabbix_1 10.10.250.230 username passwor 3306 zabbix_bak 3 > /root/zabbix_database_backup/backup.log &
